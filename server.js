643bee125f25dec6e0778db8a30d6f0da875bd12
AdamR-Work <aramosatwork@hotmail.com>
2021-03-18 17:08:58 -0500

    upvoting for posts and ability to see loggin is now generated based only if the person to logg/use

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({});
-
 
 const sequelize = require('./config/connection');
-const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -20,6 +20,9 @@ const sess = {
 };
 
 app.use(session(sess));
+
+const hbs = exphbs.create({});
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
