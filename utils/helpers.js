e50525d05aac0cd7d8639285cfe80ce7c707c9e1
AdamR-Work <aramosatwork@hotmail.com>
2021-03-20 17:41:28 -0500

    test

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,9 @@
-
-
 module.exports = {
-
+  format_date: date => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+      date
+    ).getFullYear()}`;
+  },
   format_url: url => {
     return url
       .replace('http://', '')
@@ -10,11 +12,11 @@ module.exports = {
       .split('/')[0]
       .split('?')[0];
   },
-
-    format_date: date => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-        date
-      ).getFullYear()}`;
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
     }
-  }
 
+    return word;
+  }
+};
